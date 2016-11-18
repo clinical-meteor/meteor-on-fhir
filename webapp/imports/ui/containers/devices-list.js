98d5d0d2d7a6a66db3a87fb22b5ee469643b9672
Abigail Watson <awatson1978@gmail.com>
2016-11-18 15:24:25 -0600

    devices

---
+++ webapp/imports/ui/containers/devices-list.js
@@ -0,0 +1,15 @@
+import { composeWithTracker } from 'react-komposer';
+import Devices from '../../api/devices/devices.js';
+import DevicesList from '../workflows/devices/DevicesList.js';
+import { Loading } from '../components/loading.js';
+import { Meteor } from 'meteor/meteor';
+
+const composer = (params, onData) => {
+  const subscription = Meteor.subscribe('devices');
+  if (subscription.ready()) {
+    const devices = Devices.find({},{sort: {title: -1}}).fetch();
+    onData(null, { devices });
+  }
+};
+
+export default composeWithTracker(composer, Loading)(DevicesList);
