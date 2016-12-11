01eac8ea6ef9cc0d25f24c429f065c48b6e73ff0
Abigail Watson <awatson1978@gmail.com>
2016-12-10 21:00:29 -0600

    removed OrbitalGlyph from NotFound page

--- webapp/imports/ui/pages/NotFound.js
+++ webapp/imports/ui/pages/NotFound.js
@@ -5,11 +5,12 @@ import { PageContainer } from '/imports/ui/components/PageContainer';
 import { MobilePadding } from '/imports/ui/components/MobilePadding';
 import OrbitalGlyph from '/imports/ui/components/OrbitalGlyph';
 
+// <OrbitalGlyph style={{height: "500px"}} />
+
 export const NotFound = () => (
   <div id="notFoundPage">
     <MobilePadding>
       <PageContainer>
-        <OrbitalGlyph style={{height: "500px"}} />
           <h4 style={{textAlign: "center"}}><strong>Error [404]</strong>: { window.location.pathname } does not exist.</h4>
       </PageContainer>
     </MobilePadding>
