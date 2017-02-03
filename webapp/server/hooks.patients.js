23aa2bf0621586113174a38f2ef9e23a485841fb
Abigail Watson <awatson1978@gmail.com>
2017-02-02 20:30:26 -0600

    settings.circle.json

--- webapp/server/hooks.patients.js
+++ webapp/server/hooks.patients.js
@@ -5,28 +5,36 @@ import { HTTP } from 'meteor/http';
 
 
 Patients.after.insert(function (userId, doc) {
-  HTTP.put(Meteor.settings.public.interfaces.default.channel.endpoint + '/Patient', {
-    data: doc
-  }, function(error, result){
-    if (error) {
-      console.log("POST /Patient", error);
-    }
-    if (result) {
-      console.log("POST /Patient", result);
-    }
-  });
+
+  // interface needs to be active in order to send the messages
+  if (Meteor.settings && Meteor.settings.public && Meteor.settings.public.interfaces && Meteor.settings.public.interfaces.default && Meteor.settings.public.interfaces.default.status && (Meteor.settings.public.interfaces.default.status === "active")) {
+    HTTP.put(Meteor.settings.public.interfaces.default.channel.endpoint + '/Patient', {
+      data: doc
+    }, function(error, result){
+      if (error) {
+        console.log("POST /Patient", error);
+      }
+      if (result) {
+        console.log("POST /Patient", result);
+      }
+    });
+  }
 });
 Patients.after.update(function (userId, doc) {
-  HTTP.post(Meteor.settings.public.interfaces.default.channel.endpoint + '/Patient', {
-    data: doc
-  }, function(error, result){
-    if (error) {
-      console.log("POST /Patient", error);
-    }
-    if (result) {
-      console.log("POST /Patient", result);
-    }
-  });
+
+  // interface needs to be active in order to send the messages
+  if (Meteor.settings && Meteor.settings.public && Meteor.settings.public.interfaces && Meteor.settings.public.interfaces.default && Meteor.settings.public.interfaces.default.status && (Meteor.settings.public.interfaces.default.status === "active")) {
+    HTTP.post(Meteor.settings.public.interfaces.default.channel.endpoint + '/Patient', {
+      data: doc
+    }, function(error, result){
+      if (error) {
+        console.log("POST /Patient", error);
+      }
+      if (result) {
+        console.log("POST /Patient", result);
+      }
+    });
+  }
 });
 Patients.after.remove(function (userId, doc) {
   // ...
