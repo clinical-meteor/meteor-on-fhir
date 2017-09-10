083e93f4b39fca11d95f4424560df357fc39d3b4
Abigail Watson <awatson1978@gmail.com>
2017-09-09 19:31:52 -0500

    main index

--- webapp/imports/ui/pages/MainIndex.js
+++ webapp/imports/ui/pages/MainIndex.js
@@ -124,10 +124,13 @@ export class MainIndex extends React.Component {
 
           {this.renderDiagnosticReport(this.data.user)}
           {this.renderConditions(this.data.user)}
-          {this.renderAllergyIntolerance(this.data.user)}
+          {/* {this.renderAllergyIntolerance(this.data.user)} */}
 
           {this.renderProcedures(this.data.user)}
           {this.renderImmunizations(this.data.user)}
+          {this.renderAllergies(this.data.user)}
+          {this.renderMedicationStatements(this.data.user)}
+
 
           <br/>
 
@@ -384,6 +387,27 @@ export class MainIndex extends React.Component {
       }
     }
   }
+
+
+  renderAllergies(user){
+    if (Meteor.settings.public.modules.fhir.AllergyIntolerances) {
+      if (user.isPractitioner || user.isAdmin) {
+        return (
+          <div id='immunizationsTile' style={this.data.style.indexCardPadding} onClick={ this.openLink.bind(this, '/allergies') } >
+            <GlassCard style={this.data.style.indexCard} >
+              <CardTitle
+                title='Allergy Intollerances'
+                subtitle='Things that might trigger autoimmunize responses.'
+                titleStyle={this.data.style.title}
+                subtitleStyle={this.data.style.subtitle}
+              />
+            </GlassCard>
+          </div>
+        );
+      }
+    }
+  }
+
   renderProcedures(user){
     if (Meteor.settings.public.modules.fhir.Procedures) {
       if (user.isPractitioner || user.isAdmin) {
@@ -603,6 +627,25 @@ export class MainIndex extends React.Component {
       }
     }
   }   
+  renderMedicationStatements(user){
+    if (Meteor.settings.public.modules.fhir.AllergyIntolerances) {
+      if (user.isPatient || user.isPractitioner || user.isAdmin) {
+        return (
+            <div id='medicationStatementsTile' style={this.data.style.inactiveIndexCard} onClick={ this.openLink.bind(this, '/allergies') } >
+              <GlassCard style={this.data.style.indexCard} >
+                <CardTitle
+                  title='Medication Statements'
+                  subtitle='Current list of medications a person is on.'
+                  titleStyle={this.data.style.title}
+                  subtitleStyle={this.data.style.subtitle}
+                />
+              </GlassCard>
+            </div>
+        );
+      }
+    }
+  }   
+  
 
 
   renderImagingStudy(user){
@@ -680,7 +723,20 @@ export class MainIndex extends React.Component {
               </GlassCard>
             </div>*/}
 
-            <div id='allergiesTile' style={this.data.style.indexCardPadding} onClick={ this.openLink.bind(this, '/allergies') } >
+            {/* <div id='allergiesTi
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            le' style={this.data.style.indexCardPadding} onClick={ this.openLink.bind(this, '/allergies') } >
               <GlassCard style={this.data.style.indexCard} >
                 <CardTitle
                   title='Allergies'
@@ -689,7 +745,7 @@ export class MainIndex extends React.Component {
                   subtitleStyle={this.data.style.subtitle}
                 />
               </GlassCard>
-            </div>
+            </div> */}
             <div id='bodySitesTile' style={this.data.style.indexCardPadding} onClick={ this.openLink.bind(this, '/body-sites') } >
               <GlassCard style={this.data.style.indexCard} >
                 <CardTitle
@@ -737,7 +793,7 @@ export class MainIndex extends React.Component {
             </div>    
 
 
-            <div id='medicationStatementTile' style={this.data.style.indexCardPadding} onClick={ this.openLink.bind(this, '/medication-statements') } >
+            {/* <div id='medicationStatementTile' style={this.data.style.indexCardPadding} onClick={ this.openLink.bind(this, '/medication-statements') } >
               <GlassCard style={this.data.style.indexCard} >
                 <CardTitle
                   title='Medication Statement'
@@ -746,7 +802,7 @@ export class MainIndex extends React.Component {
                   subtitleStyle={this.data.style.subtitle}
                 />
               </GlassCard>
-            </div>    
+            </div>     */}
 
             <div id='medicationOrderTile' style={this.data.style.indexCardPadding} onClick={ this.openLink.bind(this, '/medication-orders') } >
               <GlassCard style={this.data.style.indexCard} >
