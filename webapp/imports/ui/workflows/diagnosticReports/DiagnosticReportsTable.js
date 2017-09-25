fe438bd07ec0af94035f4b8a9ac5e315db1e9c0c
Abigail Watson <awatson1978@gmail.com>
2017-09-25 00:31:21 -0500

    DiagnosticReportsTable

--- webapp/imports/ui/workflows/diagnosticReports/DiagnosticReportsTable.js
+++ webapp/imports/ui/workflows/diagnosticReports/DiagnosticReportsTable.js
@@ -5,6 +5,7 @@ import { ReactMeteorData } from 'meteor/react-meteor-data';
 import ReactMixin from 'react-mixin';
 import { Table } from 'react-bootstrap';
 import { get } from 'lodash';
+import { moment } from 'meteor/momentjs:moment';
 
 export default class DiagnosticReportsTable extends React.Component {
 
@@ -21,7 +22,55 @@ export default class DiagnosticReportsTable extends React.Component {
     }
     
     if(DiagnosticReports.find().count() > 0){
-      data.diagnosticReports = DiagnosticReports.find().fetch();
+      data.diagnosticReports = DiagnosticReports.find().map(function(report){
+        var newRow = {
+          subjectDisplay: '',
+          code: '',
+          status: '',
+          issued: '',
+          performerDisplay: '',
+          identifier: '',
+          category: '',
+          effectiveDate: ''
+        };
+        if (report){
+          if(report.subject){
+            if(report.subject.display){
+              newRow.subjectDisplay = report.subject.display;
+            } else {
+              newRow.subjectDisplay = report.subject.reference;          
+            }
+          }
+  
+          if(report.performer && report.performer[0]){
+            if(report.performer.display){
+              newRow.performerDisplay = report.performer[0].display;
+            } else {
+              newRow.performerDisplay = report.performer[0].reference;          
+            }
+          }
+          if(report.code){
+            newRow.code = report.code.text;
+          }
+
+          if(report.identifier && report.identifier[0] && report.identifier[0].value){
+            newRow.identifier = report.identifier[0].value;
+          }
+          if(report.status){
+            newRow.status = report.status;
+          }
+          if(report.effectiveDateTime){
+            newRow.effectiveDate = moment(report.effectiveDateTime).format("YYYY-MM-DD");
+          }
+          if(report.category && report.category.coding && report.category.coding[0] && report.category.coding[0].code ){
+            newRow.category = report.category.coding[0].code;
+          }
+          if(report.issued){
+            newRow.issued = moment(report.issued).format("YYYY-MM-DD"); 
+          }       
+        } 
+        return newRow;  
+      });
     }
 
 
@@ -38,49 +87,18 @@ export default class DiagnosticReportsTable extends React.Component {
   render () {
     let tableRows = [];
     for (var i = 0; i < this.data.diagnosticReports.length; i++) {
-      var newRow = {
-        subjectDisplay: '',
-        code: '',
-        status: '',
-        issued: '',
-        performerDisplay: '',
-        identifier: '',
-        category: '',
-        effectiveDate: ''
-      };
-      if (this.data.diagnosticReports[i]){
-        if(this.data.diagnosticReports[i].patient.display){
-          newRow.patientDisplay = get(this, 'data.diagnosticReports[i].patient.display');
-        } else {
-          newRow.patientDisplay = get(this, 'data.diagnosticReports[i].patient.reference');          
-        }
-        if(get(this, 'data.diagnosticReports[i].performer[0].display')){
-          newRow.performerDisplay = get(this, 'data.diagnosticReports[i].performer[0].display');
-        }
-        if(get(this, 'data.diagnosticReports[i].code.text')){
-          newRow.code = get(this, 'data.diagnosticReports[i].code.text');
-        }
-        if(get(this, 'data.diagnosticReports[i].status')){
-          newRow.status = get(this, 'data.diagnosticReports[i].status');
-        }
-        if(get(this, 'data.diagnosticReports[i].issued')){
-          newRow.issued = get(this, 'data.diagnosticReports[i].issued');
-        }
-
-
-      }
 
       tableRows.push(
         <tr key={i} className="diagnosticReportRow" style={{cursor: "pointer"}} onClick={ this.rowClick.bind('this', this.data.diagnosticReports[i]._id)} >
 
-          <td className='subjectDisplay'>{ newRow.subjectDisplay }</td>
-          <td className='code'>{ newRow.code }</td>
-          <td className='status'>{ newRow.status }</td>
-          <td className='issued'>{ newRow.issued }</td>
-          <td className='performerDisplay'>{ newRow.performerDisplay }</td>
-          <td className='identifier'>{ newRow.identifier }</td>
-          <td className='category'>{ newRow.category }</td>
-          <td className='effectiveDate'>{ newRow.effectiveDate }</td>
+          <td className='subjectDisplay'>{ this.data.diagnosticReports[i].subjectDisplay }</td>
+          <td className='code'>{ this.data.diagnosticReports[i].code }</td>
+          <td className='status'>{ this.data.diagnosticReports[i].status }</td>
+          <td className='issued'>{ this.data.diagnosticReports[i].issued }</td>
+          <td className='performerDisplay'>{ this.data.diagnosticReports[i].performerDisplay }</td>
+          <td className='identifier'>{ this.data.diagnosticReports[i].identifier }</td>
+          <td className='effectiveDate'>{ this.data.diagnosticReports[i].effectiveDate }</td>
+          <td className='category'>{ this.data.diagnosticReports[i].category }</td>
         </tr>
       )
     }
@@ -94,7 +112,7 @@ export default class DiagnosticReportsTable extends React.Component {
             <th className='status'>status</th>
             <th className='issued'>issued</th>
             <th className='performerDisplay'>performer</th>
-            <th className='identifier'>evidence</th>
+            <th className='identifier'>identifier</th>
             <th className='effectiveDateTime'>date/time</th>
             <th className='category'>category</th>
           </tr>
