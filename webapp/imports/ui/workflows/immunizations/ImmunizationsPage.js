670b7b15ec383d87248f980aaa333904d152badc
Abigail Watson <awatson1978@gmail.com>
2017-09-24 22:29:18 -0500

    debugging

--- webapp/imports/ui/workflows/immunizations/ImmunizationsPage.js
+++ webapp/imports/ui/workflows/immunizations/ImmunizationsPage.js
@@ -1,126 +1,78 @@
-// category, value, unit, name, userId
-// 'Weight', '60', 'kg', 'Jane Doe', '123456789',
+import { CardText, CardTitle } from 'material-ui/Card';
+import {Tab, Tabs} from 'material-ui/Tabs';
 
+import { GlassCard } from '/imports/ui/components/GlassCard';
+import ImmunizationDetail from '/imports/ui/workflows/immunizations/ImmunizationDetail';
+import ImmunizationsTable from '/imports/ui/workflows/immunizations/ImmunizationsTable';
+import { Meteor } from 'meteor/meteor';
+import React  from 'react';
+import { ReactMeteorData } from 'meteor/react-meteor-data';
+import ReactMixin  from 'react-mixin';
+import { VerticalCanvas } from '/imports/ui/components/VerticalCanvas';
 
-
-
-module.exports = {
-  url: 'http://localhost:3000/immunizations',
-  commands: [{
-
-    verifyElements: function() {
-      return this
-        .waitForElementPresent('#immunizationsPage', 10000)
-        .verify.elementPresent('#immunizationsTable');
-    },
-    verifyEmptyList: function() {
-      return this
-        .verify.elementNotPresent('#immunizationsTable .immunizationRow:nth-child(1)');
-    },
-    verifyImmunizationListCard: function() {
-      return this
-        .verify.elementPresent('#immunizationsTable')
-        .verify.elementPresent('#immunizationsTable .immunizationRow:nth-child(1)')
-        .verify.elementPresent('#immunizationsTable .immunizationRow:nth-child(1) .identifier')
-        .verify.elementPresent('#immunizationsTable .immunizationRow:nth-child(1) .vaccine')
-        .verify.elementPresent('#immunizationsTable .immunizationRow:nth-child(1) .vaccineCode');
-    },
-    selectNewImmunizationTab: function() {
-      return this
-        .verify.elementPresent('#immunizationsPageTabs')
-        .verify.elementPresent('#immunizationsPageTabs .newImmunizationTab')
-        .click("#immunizationsPageTabs .newImmunizationTab");
-    },
-    verifyNewImmunizationCard: function() {
-      return this
-        .verify.elementPresent('#immunizationsPage .immunizationDetail')
-        .verify.elementPresent('#immunizationsPage .immunizationDetail input[name="identifier"]')
-        .verify.elementPresent('#immunizationsPage .immunizationDetail input[name="vaccine"]')
-        .verify.elementPresent('#immunizationsPage .immunizationDetail input[name="vaccineCode"]')
-    },
-    verifyImmunizationDetails: function(identifier, vaccine, vaccineCode) {
-      this
-        .waitForElementPresent('#immunizationDetails', 5000);
-
-      if (identifier) {
-        this.verify.attributeEquals('#immunizationsPage .immunizationDetail  input[name="identifier"]', 'value', identifier);
-      }
-      if (vaccine) {
-        this.verify.attributeEquals('#immunizationsPage .immunizationDetail  input[name="vaccine"]', 'value', vaccine);
-      }
-      if (vaccineCode) {
-        this.verify.attributeEquals('#immunizationsPage .immunizationDetail  input[name="vaccineCode"]', 'value', vaccineCode);
-      }
-      return this;
-    },
-    listContainsImmunization: function (index, identifier, vaccine, vaccineCode){
-      this
-        .verify.elementPresent('#immunizationsTable')
-        .verify.elementPresent('#immunizationsTable .immunizationRow:nth-child(' + index + ')')
-        .verify.elementPresent('#immunizationsTable .immunizationRow:nth-child(' + index + ') .identifier')
-        .verify.elementPresent('#immunizationsTable .immunizationRow:nth-child(' + index + ') .vaccine')
-        .verify.elementPresent('#immunizationsTable .immunizationRow:nth-child(' + index + ') .vaccineCode')
-
-      if (identifier) {
-        this.verify.containsText('#immunizationsTable .immunizationRow:nth-child(' + index + ') .identifier', identifier);
-      }
-      if (vaccine) {
-        this.verify.containsText('#immunizationsTable .immunizationRow:nth-child(' + index + ') .vaccine', vaccine);
-      }
-      if (vaccineCode) {
-        this.verify.containsText('#immunizationsTable .immunizationRow:nth-child(' + index + ') .vaccineCode', vaccineCode);
-      }
-      return this;
-    },
-    selectListTab: function(){
-      return this.click('#immunizationsPage .immunizationListTab');
-    },
-    displayListCard: function(){
-      return this.click('#immunizationsPage .immunizationListTab');
-    },
-    displayNewImmunizationCard: function(){
-      return this.click('#immunizationsPage .newImmunizationTab');
-    },
-    displayImmunizationDetails: function(){
-      return this.click('#immunizationsPage .immunizationDetailsTab');
-    },
-    selectImmunization: function(index){
-      return this.click('#immunizationsTable .immunizationRow:nth-child(' + index + ')');
-    },
-    upsertImmunization: function(identifier, vaccine, vaccineCode, pageElement) {
-      if (identifier) {
-        var identifierArray = identifier.split('');
-        for (var i = 0; i < identifierArray.length; i++) {
-          this.setValue(pageElement + ' input[name="identifier"]', identifierArray[i]);
+export class ImmunizationsPage extends React.Component {
+  getMeteorData() {
+    let data = {
+      style: {
+        opacity: Session.get('globalOpacity'),
+        tab: {
+          borderBottom: '1px solid lightgray',
+          borderRight: 'none'
         }
-      }
-      if (vaccine) {
-        var vaccineArray = vaccine.split('');
-        for (var k = 0; k < vaccineArray.length; k++) {
-          this.setValue(pageElement + ' input[name="vaccine"]', vaccineArray[k]);
-        }
-      }
-      if (vaccineCode) {
-        var vaccineCodeArray = vaccineCode.split('');
-        for (var j = 0; j < vaccineCodeArray.length; j++) {
-          this.setValue(pageElement + ' input[name="vaccineCode"]', vaccineCodeArray[j]);
-        }
-      }
+      },
+      state: {
+        isLoggedIn: false
+      },
+      tabIndex: Session.get('immunizationPageTabIndex'),
+      immunizationSearchFilter: Session.get('immunizationSearchFilter'),
+      currentImmunization: Session.get('selectedImmunization')
+    };
 
-      return this;
-    },
-    saveImmunization: function(){
-      return this.verify.elementPresent('#saveImmunizationButton').click('#saveImmunizationButton');
-    },
-
-    pause: function(time, client) {
-      client.pause(time);
-      return this;
-    },
-    saveScreenshot: function(path, client){
-      client.saveScreenshot(path);
-      return this;
+    if (Meteor.user()) {
+      data.state.isLoggedIn = true;
     }
-  }],
-  elements: {}
-};
+
+    data.style = Glass.blur(data.style);
+    data.style.appbar = Glass.darkroom(data.style.appbar);
+    data.style.tab = Glass.darkroom(data.style.tab);
+
+    return data;
+  }
+
+  handleTabChange(index){
+    Session.set('immunizationPageTabIndex', index);
+  }
+
+  onNewTab(){
+    Session.set('selectedImmunization', false);
+    Session.set('immunizationUpsert', false);
+  }
+
+  render() {
+    if(process.env.NODE_ENV === "test") console.log('In ImmunizationsPage render');
+    return (
+      <div id='immunizationsPage'>
+        <VerticalCanvas>
+          <GlassCard height='auto'>
+            <CardTitle title='Immunizations' />
+            <CardText>
+              <Tabs id="immunizationsPageTabs" default value={this.data.tabIndex} onChange={this.handleTabChange} initialSelectedIndex={1}>
+               <Tab className='newImmunizationTab' label='New' style={this.data.style.tab} onActive={ this.onNewTab } value={0}>
+                 <ImmunizationDetail id='newImmunization' />
+               </Tab>
+               <Tab className="immunizationListTab" label='Immunizations' onActive={this.handleActive} style={this.data.style.tab} value={1}>
+                <ImmunizationsTable />
+               </Tab>
+               <Tab className="immunizationDetailsTab" label='Detail' onActive={this.handleActive} style={this.data.style.tab} value={2}>
+                 <ImmunizationDetail id='immunizationDetails' />
+               </Tab>
+             </Tabs>
+            </CardText>
+          </GlassCard>
+        </VerticalCanvas>
+      </div>
+    );
+  }
+}
+
+ReactMixin(ImmunizationsPage.prototype, ReactMeteorData);
