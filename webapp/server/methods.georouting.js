eb657220689894c21076da2ef9f9512af3ddee8a
Abigail Watson <awatson1978@gmail.com>
2017-10-03 20:12:15 -0500

    runs without clinical:routing-algorithms package

--- webapp/server/methods.georouting.js
+++ webapp/server/methods.georouting.js
@@ -1,31 +1,35 @@
-import {
-  clearCities,
-  createSampleCities,
-  dropGraphDatabase,
-  echoAllLocations,
-  echoCities,
-  fetchCitesFromGraphDatabase,
-  generateDistances,
-  getDistance,
-  getRelationships,
-  relationshipTypes,
-  simpleSalespersonAlgorithm
-} from 'meteor/clinical:routing-algorithms';
 
-Meteor.methods({
-  dropGraphDatabase: function(){
-    dropGraphDatabase();
-  },
-  echoCities: function(){
-    echoCities();
-  },
-  createSampleCities: function(){
-    createSampleCities();
-  },
-  generateDistances: function(){
-    generateDistances();
-  },
-  simpleSalespersonAlgorithm: function(){
-    simpleSalespersonAlgorithm();
-  },
-});
+
+if(Package['clinical:routing-algorithms']){
+  import {
+    clearCities,
+    createSampleCities,
+    dropGraphDatabase,
+    echoAllLocations,
+    echoCities,
+    fetchCitesFromGraphDatabase,
+    generateDistances,
+    getDistance,
+    getRelationships,
+    relationshipTypes,
+    simpleSalespersonAlgorithm
+  } from 'meteor/clinical:routing-algorithms';
+  
+  Meteor.methods({
+    dropGraphDatabase: function(){
+      dropGraphDatabase();
+    },
+    echoCities: function(){
+      echoCities();
+    },
+    createSampleCities: function(){
+      createSampleCities();
+    },
+    generateDistances: function(){
+      generateDistances();
+    },
+    simpleSalespersonAlgorithm: function(){
+      simpleSalespersonAlgorithm();
+    },
+  });    
+}
