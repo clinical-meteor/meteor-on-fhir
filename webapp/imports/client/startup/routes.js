27a4fb4a66bb840308632739571295ddb834c21b
Abigail Watson <awatson1978@gmail.com>
2018-01-19 18:30:09 -0600

    meteor remove symptomatic:blockchain-core

--- webapp/imports/client/startup/routes.js
+++ webapp/imports/client/startup/routes.js
@@ -256,7 +256,7 @@ Meteor.startup(() => {
 
               <Route name="TimelinePage" path="/timeline" component={ TimelinePage }  onEnter={ requireAuth }/>
               
-              { dynamicRoutes.map(route => <Route name={route.name} path={route.path} component={ route.component } />) }
+              { dynamicRoutes.map(function(route, index){<Route name={route.name} path={route.path} component={ route.component } key={index}/> })}
                     
               <Route name="timeline" path="/timeline-sidescroll" component={ TimelineSidescrollPage }  />
 
