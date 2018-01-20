59998ce8cf7b082ec5c48b3ffe8d2f77c8599bf5
Abigail Watson <awatson1978@gmail.com>
2018-01-20 08:57:41 -0600

    prep_for_circle.sh

---
+++ scripts/prep_for_circle_ci.sh
@@ -0,0 +1,20 @@
+#!/bin/sh  
+
+
+# remove symptomatic plugins
+meteor remove symptomatic:blockchain-core
+meteor remove symptomatic:landing-page
+meteor remove symptomatic:videoconferencing
+meteor remove symptomatic:vault-server
+meteor remove symptomatic:continuity-of-care
+meteor remove symptomatic:grid-layout
+meteor remove symptomatic:static-pages
+meteor remove symptomatic:routing-algorithms
+
+# add default plugins
+meteor add clinical:plugin-default-landing-page
+
+
+
+
+
