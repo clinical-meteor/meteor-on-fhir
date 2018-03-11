47eb3101418433604c53e4ce31a37b61a0508f60
Abigail Watson <abigailwatson@Abigails-Mac-mini-96320.local>
2018-03-11 12:32:29 -0500

    selenium 3.4.0

--- webapp/imports/startup/server/security.js
+++ webapp/imports/startup/server/security.js
@@ -11,15 +11,85 @@ import { _ } from 'meteor/underscore';
 
 // // Don't let people write arbitrary data to their 'profile' field from the client
 Patients.allow({
-  update() {
-    return true;
-  },
-  insert() {
-    return true;
-  },
-  remove() {
-    return true;
-  },
+  update() { return true; },
+  insert() { return true; },
+  remove() { return true; }
+});
+
+AllergyIntolerances.allow({
+  update() { return true; },
+  insert() { return true; },
+  remove() { return true; }
+});
+Conditions.allow({
+  update() { return true; },
+  insert() { return true; },
+  remove() { return true; }
+});
+Devices.allow({
+  update() { return true; },
+  insert() { return true; },
+  remove() { return true; }
+});
+DiagnosticReports.allow({
+  update() { return true; },
+  insert() { return true; },
+  remove() { return true; }
+});
+Goals.allow({
+  update() { return true; },
+  insert() { return true; },
+  remove() { return true; }
+});
+Immunizations.allow({
+  update() { return true; },
+  insert() { return true; },
+  remove() { return true; }
+});
+// Locations.allow({
+//   update() { return true; },
+//   insert() { return true; },
+//   remove() { return true; }
+// });
+Medications.allow({
+  update() { return true; },
+  insert() { return true; },
+  remove() { return true; }
+});
+MedicationStatements.allow({
+  update() { return true; },
+  insert() { return true; },
+  remove() { return true; }
+});
+Observations.allow({
+  update() { return true; },
+  insert() { return true; },
+  remove() { return true; }
+});
+Organizations.allow({
+  update() { return true; },
+  insert() { return true; },
+  remove() { return true; }
+});
+Patients.allow({
+  update() { return true; },
+  insert() { return true; },
+  remove() { return true; }
+});
+Practitioners.allow({
+  update() { return true; },
+  insert() { return true; },
+  remove() { return true; }
+});
+Procedures.allow({
+  update() { return true; },
+  insert() { return true; },
+  remove() { return true; }
+});
+RiskAssessments.allow({
+  update() { return true; },
+  insert() { return true; },
+  remove() { return true; }
 });
 
 // Get a list of all accounts methods by running `Meteor.server.method_handlers` in meteor shell
