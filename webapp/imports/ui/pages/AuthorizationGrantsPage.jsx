d58024ae0753c9e09fc22d8f9ff23a9cfa30c1b5
Abigail Watson <abigailwatson@Abigails-Mac-mini-103623.local>
2018-03-21 20:32:39 -0500

    profile page redesign; oauth-grants

--- webapp/imports/ui/pages/AuthorizationGrantsPage.jsx
+++ webapp/imports/ui/pages/AuthorizationGrantsPage.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 import { AboutAppCard } from '/imports/ui/components/AboutAppCard';
-import { VerticalCanvas, GlassCard } from 'meteor/clinical:glass-ui';
+import { VerticalCanvas, GlassCard, DynamicSpacer } from 'meteor/clinical:glass-ui';
 import { CardActions, CardHeader, CardText, CardTitle } from 'material-ui/Card';
 
 import { ConsentTable } from 'meteor/clinical:hl7-resource-consent'
+import RaisedButton from 'material-ui/RaisedButton';
 
 export class AuthorizationGrantsPage extends React.Component {
   constructor(props) {
@@ -15,7 +16,78 @@ export class AuthorizationGrantsPage extends React.Component {
     return(
       <div id="authorizationGrantsPage">
         <VerticalCanvas >
-          <GlassCard height='auto' width='768px'>
+          <GlassCard width='768px'>
+            <CardTitle title="Advanced Care Directives" subtitle='Consents, living wills, advanced care directives, and other legal documents.' />
+              <CardText>
+              <RaisedButton
+                  // id='changePasswordButton'
+                  label='Consent to View Personal Health Info'
+                  // onClick={this.changePassword.bind(this)}
+                  className="muidocs-icon-action-delete"
+                  primary={true}
+                  style={{minWidth: '400px'}}
+                  /><br/><br/>
+              {/* <RaisedButton
+                  // id='changePasswordButton'
+                  label='Consent to View Mental Health Data'
+                  // onClick={this.changePassword.bind(this)}
+                  className="muidocs-icon-action-delete"
+                  primary={true}
+                  style={{minWidth: '400px'}}
+                  /><br/><br/>                   */}
+              <RaisedButton
+                  // id='changePasswordButton'
+                  label='Consent to Treat'
+                  // onClick={this.changePassword.bind(this)}
+                  className="muidocs-icon-action-delete"
+                  primary={true}
+                  style={{minWidth: '400px'}}
+                  /><br/><br/>     
+              <RaisedButton
+                  // id='changePasswordButton'
+                  label='OAuth 2.0 Consent'
+                  // onClick={this.changePassword.bind(this)}
+                  className="muidocs-icon-action-delete"
+                  primary={true}
+                  style={{minWidth: '400px'}}
+                  /><br/><br/>
+              <RaisedButton
+                  // id='changePasswordButton'
+                  label='My Final Days'
+                  // onClick={this.changePassword.bind(this)}
+                  className="muidocs-icon-action-delete"
+                  primary={true}
+                  style={{minWidth: '400px'}}
+                  /><br/><br/>
+              <RaisedButton
+                  // id='changePasswordButton'
+                  label='Quality of Life'
+                  // onClick={this.changePassword.bind(this)}
+                  className="muidocs-icon-action-delete"
+                  primary={true}
+                  style={{minWidth: '400px'}}
+                  /><br/><br/>                  
+              <RaisedButton
+                  // id='changePasswordButton'
+                  label='Organ Donations'
+                  // onClick={this.changePassword.bind(this)}
+                  className="muidocs-icon-action-delete"
+                  primary={true}
+                  style={{minWidth: '400px'}}
+                  /><br/><br/>                             
+              <RaisedButton
+                  // id='changePasswordButton'
+                  label='Organ Donations'
+                  // onClick={this.changePassword.bind(this)}
+                  className="muidocs-icon-action-delete"
+                  primary={true}
+                  style={{minWidth: '400px'}}
+                  /><br/><br/>                                   
+            </CardText>              
+          </GlassCard>
+          <DynamicSpacer />
+
+          <GlassCard width='768px'>
             <CardTitle title="Consents & Authorizations" subtitle='OAuth tokens, HIPAA consents, Advanced Directives, etc.' />
               <CardText>
                 <ConsentTable
@@ -24,6 +96,7 @@ export class AuthorizationGrantsPage extends React.Component {
                 />
             </CardText>              
           </GlassCard>
+
         </VerticalCanvas>
       </div>
     );
