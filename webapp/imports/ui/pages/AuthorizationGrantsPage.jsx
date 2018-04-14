c354c5dd5f667f8c329ebe069dcce7909040101b
Abigail Watson <abigailwatson@Abigails-Mac-mini-317685.local>
2018-04-14 02:11:33 -0500

    sync

--- webapp/imports/ui/pages/AuthorizationGrantsPage.jsx
+++ webapp/imports/ui/pages/AuthorizationGrantsPage.jsx
@@ -5,6 +5,7 @@ import { VerticalCanvas, GlassCard, DynamicSpacer } from 'meteor/clinical:glass-
 import { CardActions, CardHeader, CardText, CardTitle } from 'material-ui/Card';
 
 import RaisedButton from 'material-ui/RaisedButton';
+import { Col, Grid, Row } from 'react-bootstrap';
 
 if(Package['clinical:hl7-resource-consent']){
   import { ConsentTable } from 'meteor/clinical:hl7-resource-consent';
@@ -18,13 +19,13 @@ export class AuthorizationGrantsPage extends React.Component {
   render(){
 
     var consentElement;
-    if(Package['clinical:hl7-resource-consent']){
+    // if(Package['clinical:hl7-resource-consent']){
       consentElement = <ConsentTable
         simplified={ true }
         patient="Jane Doe"
         noDataMessage={false}
       />
-    }
+    // }
 
     return(
       <div id="authorizationGrantsPage">
@@ -32,70 +33,80 @@ export class AuthorizationGrantsPage extends React.Component {
           <GlassCard width='768px'>
             <CardTitle title="Advanced Care Directives" subtitle='Consents, living wills, advanced care directives, and other legal documents.' />
               <CardText>
-              <RaisedButton
-                  // id='changePasswordButton'
-                  label='Consent to View Personal Health Info'
-                  // onClick={this.changePassword.bind(this)}
-                  className="muidocs-icon-action-delete"
-                  primary={true}
-                  style={{minWidth: '400px'}}
-                  /><br/><br/>
-              {/* <RaisedButton
-                  // id='changePasswordButton'
-                  label='Consent to View Mental Health Data'
-                  // onClick={this.changePassword.bind(this)}
-                  className="muidocs-icon-action-delete"
-                  primary={true}
-                  style={{minWidth: '400px'}}
-                  /><br/><br/>                   */}
-              <RaisedButton
-                  // id='changePasswordButton'
-                  label='Consent to Treat'
-                  // onClick={this.changePassword.bind(this)}
-                  className="muidocs-icon-action-delete"
-                  primary={true}
-                  style={{minWidth: '400px'}}
-                  /><br/><br/>     
-              <RaisedButton
-                  // id='changePasswordButton'
-                  label='OAuth 2.0 Consent'
-                  // onClick={this.changePassword.bind(this)}
-                  className="muidocs-icon-action-delete"
-                  primary={true}
-                  style={{minWidth: '400px'}}
-                  /><br/><br/>
-              <RaisedButton
-                  // id='changePasswordButton'
-                  label='My Final Days'
-                  // onClick={this.changePassword.bind(this)}
-                  className="muidocs-icon-action-delete"
-                  primary={true}
-                  style={{minWidth: '400px'}}
-                  /><br/><br/>
-              <RaisedButton
-                  // id='changePasswordButton'
-                  label='Quality of Life'
-                  // onClick={this.changePassword.bind(this)}
-                  className="muidocs-icon-action-delete"
-                  primary={true}
-                  style={{minWidth: '400px'}}
-                  /><br/><br/>                  
-              <RaisedButton
-                  // id='changePasswordButton'
-                  label='Organ Donations'
-                  // onClick={this.changePassword.bind(this)}
-                  className="muidocs-icon-action-delete"
-                  primary={true}
-                  style={{minWidth: '400px'}}
-                  /><br/><br/>                             
-              <RaisedButton
-                  // id='changePasswordButton'
-                  label='Organ Donations'
-                  // onClick={this.changePassword.bind(this)}
-                  className="muidocs-icon-action-delete"
-                  primary={true}
-                  style={{minWidth: '400px'}}
-                  /><br/><br/>                                   
+                <Grid>
+                  <Row>
+                  <Col md={6}>
+                    <RaisedButton
+                      // id='changePasswordButton'
+                      label='Consent to View Personal Health Info'
+                      // onClick={this.changePassword.bind(this)}
+                      className="muidocs-icon-action-delete"
+                      primary={true}
+                      style={{minWidth: '400px'}}
+                      /><br/><br/>
+                    {/* <RaisedButton
+                        // id='changePasswordButton'
+                        label='Consent to View Mental Health Data'
+                        // onClick={this.changePassword.bind(this)}
+                        className="muidocs-icon-action-delete"
+                        primary={true}
+                        style={{minWidth: '400px'}}
+                        /><br/><br/>                   */}
+                    <RaisedButton
+                        // id='changePasswordButton'
+                        label='Consent to Treat'
+                        // onClick={this.changePassword.bind(this)}
+                        className="muidocs-icon-action-delete"
+                        primary={true}
+                        style={{minWidth: '400px'}}
+                        /><br/><br/>     
+                    <RaisedButton
+                        // id='changePasswordButton'
+                        label='OAuth 2.0 Consent'
+                        // onClick={this.changePassword.bind(this)}
+                        className="muidocs-icon-action-delete"
+                        primary={true}
+                        style={{minWidth: '400px'}}
+                        /><br/><br/>
+                    <RaisedButton
+                        // id='changePasswordButton'
+                        label='My Final Days'
+                        // onClick={this.changePassword.bind(this)}
+                        className="muidocs-icon-action-delete"
+                        primary={true}
+                        style={{minWidth: '400px'}}
+                        /><br/><br/>
+                  </Col>
+                  <Col md={6}>
+                    <RaisedButton
+                        // id='changePasswordButton'
+                        label='Quality of Life'
+                        // onClick={this.changePassword.bind(this)}
+                        className="muidocs-icon-action-delete"
+                        primary={true}
+                        style={{minWidth: '400px'}}
+                        /><br/><br/>                  
+                    <RaisedButton
+                        // id='changePasswordButton'
+                        label='Organ Donations'
+                        // onClick={this.changePassword.bind(this)}
+                        className="muidocs-icon-action-delete"
+                        primary={true}
+                        style={{minWidth: '400px'}}
+                        /><br/><br/>                             
+                    <RaisedButton
+                        // id='changePasswordButton'
+                        label='Organ Donations'
+                        // onClick={this.changePassword.bind(this)}
+                        className="muidocs-icon-action-delete"
+                        primary={true}
+                        style={{minWidth: '400px'}}
+                        /><br/><br/>                                   
+
+                  </Col>
+
+                  </Row>
+                </Grid>
             </CardText>              
           </GlassCard>
           <DynamicSpacer />
