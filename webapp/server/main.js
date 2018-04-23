a8eeb16760cb541b5bf822526efb5856c2f598ce
Abigail Watson <abigailwatson@Abigails-Mac-mini-317685.local>
2018-04-23 14:24:28 -0500

    AppOrchard works on local

--- webapp/server/main.js
+++ webapp/server/main.js
@@ -1,7 +1,7 @@
 import '/imports/startup/server';
 import '/imports/api/users/methods';
 
-import { BrowserPolicy } from 'meteor/browser-policy-common';
+// import { BrowserPolicy } from 'meteor/browser-policy-common';
 
 Meteor.startup(function(){
   console.log('Meteor app framework is initializing....');
@@ -15,21 +15,28 @@ Meteor.startup(function(){
     Meteor.settings.public.version = {};
   }
 
-  console.log('Configuring content-security-policy:');
-  BrowserPolicy.content.allowSameOriginForAll();
-  BrowserPolicy.content.allowDataUrlForAll()
-  BrowserPolicy.content.allowOriginForAll('self');
-  BrowserPolicy.content.allowObjectOrigin('self')
-  BrowserPolicy.content.allowOriginForAll('font src');
-  BrowserPolicy.content.allowOriginForAll('*.wikipedia.com');
-  BrowserPolicy.content.allowOriginForAll('*.wikipedia.org');
-  BrowserPolicy.content.allowOriginForAll('fonts.googleapis.com');
-  BrowserPolicy.content.allowOriginForAll('fonts.gstatic.com');
-  BrowserPolicy.content.allowImageOrigin("*")
-  BrowserPolicy.content.allowEval();
-  BrowserPolicy.content.allowInlineScripts()
-  BrowserPolicy.content.allowInlineStyles()  
-  
-  BrowserPolicy.content.allowOriginForAll('*.ourcarewishes.org');
+  if(Package['browser-policy']){
+    console.log('Configuring content-security-policy:');
+    BrowserPolicy.content.allowSameOriginForAll();
+    BrowserPolicy.content.allowDataUrlForAll()
+    BrowserPolicy.content.allowOriginForAll('self');
+    BrowserPolicy.content.allowObjectOrigin('self')
+    BrowserPolicy.content.allowOriginForAll('font src');
+    BrowserPolicy.content.allowOriginForAll('*.wikipedia.com');
+    BrowserPolicy.content.allowOriginForAll('*.wikipedia.org');
+    BrowserPolicy.content.allowOriginForAll('fonts.googleapis.com');
+    BrowserPolicy.content.allowOriginForAll('fonts.gstatic.com');
+    BrowserPolicy.content.allowImageOrigin("*")
+    BrowserPolicy.content.allowEval();
+    BrowserPolicy.content.allowInlineScripts()
+    BrowserPolicy.content.allowInlineStyles()  
+    
+    BrowserPolicy.content.allowOriginForAll('*.ourcarewishes.org');
+  }
 
+    // This will load up the registered OAuth services into active memory on the server
+    if(Package['clinical:smart-on-fhir-client']){
+      console.log('Loading OAuth configuration from Mongo into active server memory.')
+      Meteor.call('resyncConfiguration');
+    }
 })
