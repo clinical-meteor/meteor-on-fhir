35a1463c670840e3cdbce0e88d6987368bd84d28
Abigail Watson <abigailwatson@Abigails-Mac-mini-317772.local>
2018-05-11 12:16:22 -0500

    diagnostics

--- scripts/production_build_deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh  
-
-cp -R bundle/* ourcarewishes_fhir/
-
-
-
-
--- scripts/version.sh
+++ scripts/version.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-ver=$(git describe --abbrev=0)
-complete=$(git describe)
+ver=$(git describe --abbrev=0 --tags)
+complete=$(git describe --tags)
 branch=$(git rev-parse --abbrev-ref HEAD)
 commit=$(git rev-parse HEAD)
 timestamp=$(git log -1 --date=short --pretty=format:%cd)
