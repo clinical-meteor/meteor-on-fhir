4204ecc19f0a6d4b3393cfeb397701ff5b0b60a5
Abigail Watson <awatson1978@gmail.com>
2018-05-19 13:28:23 -0500

    browser-policy-common optional

--- webapp/server/main.js
+++ webapp/server/main.js
@@ -1,7 +1,6 @@
 import '/imports/startup/server';
 import '/imports/api/users/methods';
 
-import { BrowserPolicy } from 'meteor/browser-policy-common';
 
 Meteor.startup(function(){
 
@@ -20,24 +19,29 @@ Meteor.startup(function(){
   }
 
   console.log('Configuring content-security-policy:');
-  BrowserPolicy.content.allowSameOriginForAll();
-  BrowserPolicy.content.allowDataUrlForAll()
-  BrowserPolicy.content.allowOriginForAll('self');
-  BrowserPolicy.content.allowObjectOrigin('self')
-  BrowserPolicy.content.allowOriginForAll('font src');
-  BrowserPolicy.content.allowOriginForAll('*.wikipedia.com');
-  BrowserPolicy.content.allowOriginForAll('*.wikipedia.org');
-  BrowserPolicy.content.allowOriginForAll('fonts.googleapis.com');
-  BrowserPolicy.content.allowOriginForAll('fonts.gstatic.com');
-  BrowserPolicy.content.allowImageOrigin("*")
-  BrowserPolicy.content.allowEval();
-  BrowserPolicy.content.allowInlineScripts()
-  BrowserPolicy.content.allowInlineStyles()  
 
-  BrowserPolicy.content.allowObjectOrigin( 'zygotebody.com' );
-  BrowserPolicy.content.allowFrameOrigin('zygotebody.com');
-  BrowserPolicy.content.allowObjectDataUrl('zygotebody.com');
-  BrowserPolicy.content.allowOriginForAll('zygotebody.com');
-  BrowserPolicy.content.allowConnectOrigin("zygotebody.com")
-  BrowserPolicy.content.allowImageOrigin("zygotebody.com")   
+  if(Package['browser-policy-common']){
+    import { BrowserPolicy } from 'meteor/browser-policy-common';
+
+    BrowserPolicy.content.allowSameOriginForAll();
+    BrowserPolicy.content.allowDataUrlForAll()
+    BrowserPolicy.content.allowOriginForAll('self');
+    BrowserPolicy.content.allowObjectOrigin('self')
+    BrowserPolicy.content.allowOriginForAll('font src');
+    BrowserPolicy.content.allowOriginForAll('*.wikipedia.com');
+    BrowserPolicy.content.allowOriginForAll('*.wikipedia.org');
+    BrowserPolicy.content.allowOriginForAll('fonts.googleapis.com');
+    BrowserPolicy.content.allowOriginForAll('fonts.gstatic.com');
+    BrowserPolicy.content.allowImageOrigin("*")
+    BrowserPolicy.content.allowEval();
+    BrowserPolicy.content.allowInlineScripts()
+    BrowserPolicy.content.allowInlineStyles()  
+  
+    BrowserPolicy.content.allowObjectOrigin( 'zygotebody.com' );
+    BrowserPolicy.content.allowFrameOrigin('zygotebody.com');
+    BrowserPolicy.content.allowObjectDataUrl('zygotebody.com');
+    BrowserPolicy.content.allowOriginForAll('zygotebody.com');
+    BrowserPolicy.content.allowConnectOrigin("zygotebody.com")
+    BrowserPolicy.content.allowImageOrigin("zygotebody.com");
+  }
 })
