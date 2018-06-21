75f88679ab42618ce29640555536e1f57c9056be
gameltzer <gameltzer@gmail.com>
2018-06-21 17:36:28 -0500

    HOpefully this works with the ideogram file.

---
+++ webapp/ideogram/server/methods.js
@@ -0,0 +1,21 @@
+Meteor.methods({
+    // createIdeogram: function(text){
+    //     check(text, String);
+    //     console.log('createIdeogram()', text);
+    //     //probably will have to define collection Ideograms
+    //     Ideograms.insert({
+    //         //probably will have to define resourceType Ideogram
+    //         resourceType: 'Ideogram',
+    //         Ideogram:text
+    //         })
+    // }
+    // createNewNote: function(text){
+    //     check(text, String);        
+    //     console.log('createNewNote()', text);
+
+    //     Notes.insert({
+    //         resourceType: 'Note',
+    //         note: text
+    //     });
+    // }
+});
