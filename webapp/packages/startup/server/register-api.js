3daec9302d2c7a468b24deae5d8351994e8893f1
Abigail Watson <abigailwatson@Abigails-Mac-mini-317772.local>
2018-06-30 13:40:11 -0500

    no circle caching

--- webapp/packages/startup/server/register-api.js
+++
@@ -1,5 +0,0 @@
-import '../../api/lists/methods.js';
-import '../../api/lists/server/publications.js';
-import '../../api/todos/methods.js';
-import '../../api/todos/server/publications.js';
-import '../../api/languages/methods.js';
