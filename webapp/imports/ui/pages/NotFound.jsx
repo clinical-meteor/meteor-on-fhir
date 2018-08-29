8d05f5334b294e6f91a4c3a09a10e07ccbb840cc
Abigail Watson <abigailwatson@Abigails-Mac-mini-379101.local>
2018-08-29 00:46:22 -0500

    NotFound error message

--- webapp/imports/ui/pages/NotFound.jsx
+++ webapp/imports/ui/pages/NotFound.jsx
@@ -1,13 +1,46 @@
-import { Alert } from 'react-bootstrap';
+import { Alert, Grid } from 'react-bootstrap';
 import React from 'react';
-import { SciFiOrbital } from '/imports/ui/components/SciFiOrbital';
 import { VerticalCanvas } from 'meteor/clinical:glass-ui';
+
+import { get } from 'lodash';
+import { Meteor } from 'meteor/meteor';
+
 
 export const NotFound = () => (
   <div id="notFoundPage">
     <VerticalCanvas>
-        {/* <SciFiOrbital /> */}
         <h4 style={{textAlign: "center"}}><strong>Error [404]</strong>: { window.location.pathname } does not exist.</h4>
+        <br />
+
+        <Grid>
+          <Alert bsStyle="warning">
+            <strong>General Troubleshooting</strong> <br />
+            Sometimes you simply request a URL that doesn't exist.
+            <br /> <br />
+            <ul>
+              <li>
+                1.  Check the spelling of the URL.
+              </li>
+            </ul>
+            <br /><br />
+
+            <strong>Developer Troubleshooting</strong> <br />
+            Othertimes, the URL should be there, but something isn't working correctly.  
+            <br /> <br />
+            <ul>
+              <li>
+                1.  Make sure that a <strong>DynamicRoute</strong> is exported from the <strong>index.jsx</strong> file of your custom package.
+              </li>
+              <li>
+                2.  Make sure that your <strong>index.js</strong> is added as the main ES6 module of your package.
+              </li>
+              <li>
+                3.  Make sure that you've added your package to the application.
+              </li>
+            </ul>
+          </Alert>
+        </Grid>
+
     </VerticalCanvas>
   </div>
 );
