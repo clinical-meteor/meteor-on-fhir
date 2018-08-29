1a3320a8c0bb46aac3b3b360897ffad2194295cd
Abigail Watson <abigailwatson@Abigails-Mac-mini-379101.local>
2018-08-29 01:08:33 -0500

    removed symptomatic packages

--- webapp/imports/ui/pages/NotFound.jsx
+++ webapp/imports/ui/pages/NotFound.jsx
@@ -6,6 +6,7 @@ import { get } from 'lodash';
 import { Meteor } from 'meteor/meteor';
 
 
+
 export const NotFound = () => (
   <div id="notFoundPage">
     <VerticalCanvas>
@@ -14,30 +15,45 @@ export const NotFound = () => (
 
         <Grid>
           <Alert bsStyle="warning">
-            <strong>General Troubleshooting</strong> <br />
-            Sometimes you simply request a URL that doesn't exist.
-            <br /> <br />
-            <ul>
-              <li>
-                1.  Check the spelling of the URL.
-              </li>
-            </ul>
-            <br /><br />
+              <strong>General Troubleshooting</strong> <br />
+                Sometimes you simply request a URL that doesn't exist.
+                <br /> <br />
+                <ul>
+                  <li>
+                    Check the spelling of the URL.
+                  </li>
+                </ul>
+                <br />
+                Othertimes, the settings file might be pointing to the wrong location. 
+                <br /><br />
+                <ol>
+                  <li>
+                    Make sure you are using the correct settings file.
+                    <pre>meteor --settings /path/to/settings.json</pre>
+                  </li>
+                  <li>
+                    Check the default route in the settings file.
+                    <pre>Meteor.settings.public.defaults.route</pre>
+                  </li>
+                </ol>
+                <br />
 
-            <strong>Developer Troubleshooting</strong> <br />
-            Othertimes, the URL should be there, but something isn't working correctly.  
-            <br /> <br />
-            <ul>
-              <li>
-                1.  Make sure that a <strong>DynamicRoute</strong> is exported from the <strong>index.jsx</strong> file of your custom package.
-              </li>
-              <li>
-                2.  Make sure that your <strong>index.js</strong> is added as the main ES6 module of your package.
-              </li>
-              <li>
-                3.  Make sure that you've added your package to the application.
-              </li>
-            </ul>
+                <strong>Developer Troubleshooting</strong> <br />
+                If you are a developer, your package might not be exporting a route correctly.  
+                <br /> <br />
+                <ul>
+                  <li>
+                    Make sure that a <strong>DynamicRoute</strong> is exported from the <strong>index.jsx</strong> file of your custom package.
+                  </li>
+                  <li>
+                    Make sure that your <strong>index.js</strong> is added as the main ES6 module of your package.
+                  </li>
+                  <li>
+                    Make sure that you've added your package to the application.
+                  </li>
+                </ul>
+
+
           </Alert>
         </Grid>
 
