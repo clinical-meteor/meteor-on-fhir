e986002cae87a7dd9aa394fec4e4d0b4e761180c
Abigail Watson <abigailwatson@Abigails-Mac-mini-380428.local>
2018-10-19 12:24:23 -0500

    removing debugToServer()

--- webapp/server/methods.logging.js
+++ webapp/server/methods.logging.js
@@ -1,12 +1,12 @@
-import { Accounts } from 'meteor/accounts-base';
-import { Meteor } from 'meteor/meteor';
+// import { Accounts } from 'meteor/accounts-base';
+// import { Meteor } from 'meteor/meteor';
 
 
-Meteor.methods({
-  debugToServer: function(functionName, error){
-    check(functionName, String);
-    check(error, Object);
-    console.error('Error in ' + functionName)
-    console.error(error)
-  }
-});
+// Meteor.methods({
+//   debugToServer: function(functionName, error){
+//     check(functionName, String);
+//     check(error, Object);
+//     console.error('Error in ' + functionName)
+//     console.error(error)
+//   }
+// });
