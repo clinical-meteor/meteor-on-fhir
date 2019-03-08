facd4d2b762b8ea04cc168cdb8277a606c8d6463
Abigail Watson <awatson1978@gmail.com>
2019-03-08 15:17:10 -0600

    Azure

---
+++ webapp/imports/startup/server/email.resetPassword.js
@@ -0,0 +1,48 @@
+import { Accounts } from 'meteor/accounts-base';
+
+
+Accounts.emailTemplates.siteName = 'Symptomatic Timeline';
+Accounts.emailTemplates.from = 'Symptomatic <catherder@symptomatic.io>';
+
+// Accounts.emailTemplates.resetPassword.text = function(user, url) {
+//   let myId = url.substring(url.lastIndexOf('/') + 1);
+//   return "Click this link to reset your password: /reset-password/" + myId;
+// }
+
+Accounts.urls.resetPassword = function(token) {
+  return Meteor.absoluteUrl('set-password/' + token);
+};
+
+Accounts.emailTemplates.resetPassword = {
+  subject() {
+    return 'Reset your password on Symptomatic';
+  },
+  text(user, url) {
+    console.log('Accounts.emailTemplates.resetPassword.url', url);
+    
+    return `Hello!
+
+      Thank you for using Symptomatic.  Click the link below to reset your password.
+
+      ${url}
+
+      If you didn't request this email, please ignore it.
+
+      Thanks,
+      The Symptomatic team
+    `;
+  }
+//   html(user, url) {
+//     return `
+//       XXX Generating HTML emails that work across different email clients is a very complicated
+//       business that we're not going to solve in this particular example app.
+
+//       A good starting point for making an HTML email could be this responsive email boilerplate:
+//       https://github.com/leemunroe/responsive-html-email-template
+
+//       Note that not all email clients support CSS, so you might need to use a tool to inline
+//       all of your CSS into style attributes on the individual elements.
+// `
+//   }
+};
+
