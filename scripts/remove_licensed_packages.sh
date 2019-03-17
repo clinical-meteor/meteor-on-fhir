b98600aaab509107f79243ce35a1c3adc98186a2
Abigail Watson <awatson1978@gmail.com>
2019-03-17 17:53:47 -0500

    cd webapp && meteor npm run-script remove_licensed_packages

---
+++ scripts/remove_licensed_packages.sh
@@ -0,0 +1,50 @@
+#!/bin/sh  
+
+echo "Trying to remove licensed packages and other intellectual property that shouldn't be distributed in the open source build."
+
+# Caution:  Brackets need spaces around them for if/then to work.
+
+# detect packages
+a1=$(meteor list | grep 'symptomatic:blockchain-core' | wc -l)
+if [ $a1 -gt 0 ]; then echo "Removing symptomatic:blockchain-core"; else echo "Skipping..."; fi; 
+
+a2=$(meteor list | grep 'symptomatic:continuity-of-care' | wc -l)
+if [ $a2 -gt 0 ]; then echo "Removing symptomatic:continuity-of-care"; else echo "Skipping..."; fi; 
+
+a3=$(meteor list | grep 'symptomatic:landing-page' | wc -l)
+if [ $a3 -gt 0 ]; then echo "Removing symptomatic:landing-pag"; else echo "Skipping..."; fi; 
+
+a4=$(meteor list | grep 'symptomatic:grid-layout' | wc -l)
+if [ $a4 -gt 0 ]; then echo "Removing symptomatic:grid-layout"; else echo "Skipping..."; fi; 
+
+a5=$(meteor list | grep 'symptomatic:routing-algorithms' | wc -l)
+if [ $a5 -gt 0 ]; then echo "Removing symptomatic:routing-algorithms"; else echo "Skipping..."; fi; 
+
+a6=$(meteor list | grep 'symptomatic:static-pages' | wc -l)
+if [ $a6 -gt 0 ]; then echo "Removing symptomatic:static-pages"; else echo "Skipping..."; fi; 
+
+a7=$(meteor list | grep 'symptomatic:theming' | wc -l)
+if [ $a7 -gt 0 ]; then echo "Removing symptomatic:theming"; else echo "Skipping..."; fi; 
+
+a8=$(meteor list | grep 'symptomatic:videoconferencing' | wc -l)
+if [ $a8 -gt 0 ]; then echo "Removing symptomatic:videoconferencing"; else echo "Skipping..."; fi; 
+
+a9=$(meteor list | grep 'symptomatic:vault-server' | wc -l)
+if [ $a9 -gt 0 ]; then echo "Removing symptomatic:vault-server"; else echo "Skipping..."; fi; 
+
+a10=$(meteor list | grep 'symptomatic:smart-on-fhir-client' | wc -l)
+if [ $a10 -gt 0 ]; then echo "Removing symptomatic:smart-on-fhir-client"; else echo "Skipping..."; fi; 
+
+a11=$(meteor list | grep 'clinical:plugin-default-landing-page' | wc -l)
+if [ $a11 -gt 0 ]; then echo "Removing clinical:plugin-default-landing-page"; else echo "Skipping..."; fi; 
+
+a12=$(meteor list | grep 'symptomatic:smart-on-fhir-config' | wc -l)
+if [ $a12 -gt 0 ]; then echo "Removing symptomatic:smart-on-fhir-config"; else echo "Skipping..."; fi; 
+
+a13=$(meteor list | grep 'symptomatic:geomapping' | wc -l)
+if [ $a13 -gt 0 ]; then echo "Removing symptomatic:geomapping"; else echo "Skipping..."; fi; 
+
+a14=$(meteor list | grep 'symptomatic:ihealth-cloud-api' | wc -l)
+if [ $a14 -gt 0 ]; then echo "Removing symptomatic:ihealth-cloud-api"; else echo "Skipping..."; fi; 
+
+
